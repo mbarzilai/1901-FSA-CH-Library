fc1947f62937a27dbd21cdaba07038fbfb782fe6
Priti Patel <pdpatel2@gmail.com>
2019-01-11 10:18:57 -0600

    blinker example

---
+++ 01-junior-phase/05-game-of-life/lecture-demo/blinker.js
@@ -0,0 +1,28 @@
+const addBlinker = (text) => {
+  const newBlinker = document.createElement("h1")
+  newBlinker.innerText = text
+  newBlinker.style.border = "5px solid red"
+
+  setInterval(function() {
+    newBlinker.classList.toggle('blinker-on')
+  }, 1000)
+
+  // newBlinker.addEventListener('click', function() {
+  //   this.classList.toggle('blinker-on')
+  // })
+
+  // document.getElementsByTagName('body')[0].addEventListener('click', function(event) {
+  //   console.log('event target', event.target)
+  //   console.log('this body', this)
+  // })
+
+  const item = document.getElementById('item')
+  item.appendChild(newBlinker)
+}
+
+// addBlinker('first item')
+// const addDay = (day) => {
+//   addBlinker(day)
+// }
+const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+days.forEach(newBlinker)
